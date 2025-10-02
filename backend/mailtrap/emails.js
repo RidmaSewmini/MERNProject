3ab4011de87e1c3c614c29e6a0ac8349a98ddab4
Achindu Wijeratne <achindudswijeratne@gmail.com>
2025-10-02 18:13:07 +0530

    02.10.2025 commit

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -31,7 +31,7 @@ export const sendWelcomeEmail = async (email, firstName) => {
     const response = await mailtrapClient.send({
       from: sender,
       to: recipient,
-      template_uuid: "5eb32ce6-9872-463a-8485-a48dbde1d1b7",
+      template_uuid: "08fd7c2a-9aa3-4ecd-bc13-282b197c12b7",
       template_variables: {
         company_info_name: "TechSphere Lanka",
         firstName,   // ✅ matches {{firstName}}
